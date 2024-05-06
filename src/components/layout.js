e27c571488df8921af7e5a122c4e7e46c505a010
Victor (Vigge) B. <vigge15@proton.me>
2024-05-06 10:56:33 +0200

    Done for now

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,5 @@
-import { Inter } from "next/font/google";
-
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
