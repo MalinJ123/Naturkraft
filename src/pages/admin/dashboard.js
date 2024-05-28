d7acd4c99225e1eaa4be679d9cefa481b051cdca
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2024-05-28 13:57:36 +0200

    try to fix loading function

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -784,26 +784,6 @@ export default function LoggedInStart() {
           </Box>
         </DialogActions>
       </Dialog>
-      {/* <Dialog>
-      <Box sx={{ position: "relative", display: "inline-flex"}}>
-            <CircularProgress variant="determinate"/>
-            <Box sx={{
-                  top: 0, 
-                  left: 0, 
-                  bottom: 0, 
-                  right: 0, 
-                  position: "absolute", 
-                  display: "flex",
-                  alignItems: "center", 
-                  justifyContent: "center"
-            }}>
-              <Typography variant="caption" component="div" color="text.secondary">
-                {`${Math.round(CircularProgress)}%`}
-              </Typography>
-
-            </Box>
-          </Box>
-      </Dialog> */}
     </Box>
   );
 }
